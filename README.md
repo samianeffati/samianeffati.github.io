f6a25bc112cfac2022373597d7f91eb650a07b8f
Web@c <amel.neffati@epitech.eu>
2019-03-25 18:42:44 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# amelneffati.github.io
---
+++ js/modal.js
@@ -0,0 +1,6 @@
+var portfolio = document.getElementById(modalbox);
+var modal = document.getElementById(modal);
+
+portfolio.onclick(function() {
+    modal.addClass('myModal');
+  });
